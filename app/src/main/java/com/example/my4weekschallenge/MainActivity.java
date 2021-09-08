be16545e19350313315d0f2b286dde6ebe1b5503
soleileun <raemong21@cau.ac.kr>
2021-09-08 17:58:14 +0900

    [Add] add right button on toolbar

--- app/src/main/java/com/example/my4weekschallenge/MainActivity.java
+++ app/src/main/java/com/example/my4weekschallenge/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar, menu);
+
+        return true;
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -30,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
                 finish();
                 return true;
             }
+            case R.id.rightButton: { // 오른쪽 상단 버튼 눌렀을 때
+                Toast.makeText(this, "rightButton", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.nextRightButton: { // 오른쪽 상단 버튼 눌렀을 때
+                Toast.makeText(this, "nextRightButton", Toast.LENGTH_SHORT).show();
+            }
+
         }
         return super.onOptionsItemSelected(item);
     }
