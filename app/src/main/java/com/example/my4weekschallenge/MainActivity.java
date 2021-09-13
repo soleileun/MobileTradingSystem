5820ccf94940c4ef87c3cb30d1d456baeb79a9c4
soleileun <raemong21@cau.ac.kr>
2021-09-13 13:54:00 +0900

    [Add] contents with GridLayout and GridViewAdapter

--- app/src/main/java/com/example/my4weekschallenge/MainActivity.java
+++ app/src/main/java/com/example/my4weekschallenge/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.my4weekschallenge.adapter.ScrollViewAdapter;
@@ -19,6 +27,12 @@ import com.example.my4weekschallenge.data.Itemlist;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private GridView gridview = null;
+    private GridViewAdapter adapter = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +45,32 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
 
-        ListView scrollView = (ListView) findViewById(R.id.scrollView);
-        ScrollViewAdapter adapter = new ScrollViewAdapter();
-        ListView.setAdapter(adapter);
-        ArrayList<Itemlist> arr = new ArrayList<>();
+        gridview = (GridView) findViewById(R.id.gridview);
+        adapter = new GridViewAdapter();
 
-        adapter.addItem(new Itemlist("haha","122,222", "233,222","2,000","1.38%","#656565",">"));
-        adapter.addItem(new Itemlist("NAVER","122,222", "233,222","2,000","1.38%","#656565","<"));
-        adapter.addItem(new Itemlist("KAKAO","122,222", "233,222","2,000","1.38%","#656565",">"));
+        //Adapter 안에 아이템의 정보 담기
+        adapter.addItem(new Itemlist("naver", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("kakao", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("TI", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
+        adapter.addItem(new Itemlist("tlqkf", "13,232,111", "22,222","3,000","4.45%","red","up"));
 
+        gridview.setAdapter(adapter);
 
-        
         findViewById(R.id.button).setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, SubActivity.class);
             startActivity(intent);
         });
-
     }
 
 
@@ -78,4 +102,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    class GridViewAdapter extends BaseAdapter {
+        ArrayList<Itemlist> items = new ArrayList<>();
+        Context context;
+
+        public void addItem(Itemlist item){
+            items.add(item);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int idx) {
+            return items.get(idx);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            String TAG = MainActivity.class.getSimpleName();
+            final Context context = viewGroup.getContext();
+            final Itemlist item = items.get(position);
+
+            if(convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.gridcell, viewGroup, false);
+
+                TextView name = (TextView) convertView.findViewById(R.id.name);
+                TextView tradeNum = (TextView) convertView.findViewById(R.id.tradeNum);
+
+                tradeNum.setText(item.getTradeNum());
+                name.setText(item.getName());
+                Log.d(TAG, "getView() - [ "+position+" ] "+item.getName());
+
+            } else {
+                View view = new View(context);
+                view = (View) convertView;
+            }
+
+            //각 아이템 선택 event
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, item.getTradeNum()+" 번 - "+item.getName()+" 입니당! ", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            return convertView;  //뷰 객체 반환
+        }
+    }
 }
--- app/src/main/java/com/example/my4weekschallenge/adapter/ScrollViewAdapter.java
+++ app/src/main/java/com/example/my4weekschallenge/adapter/ScrollViewAdapter.java
@@ -1,13 +1,16 @@
 package com.example.my4weekschallenge.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.example.my4weekschallenge.MainActivity;
 import com.example.my4weekschallenge.R;
 import com.example.my4weekschallenge.data.Itemlist;
 
@@ -37,21 +40,35 @@ public class ScrollViewAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int idx, View view, ViewGroup parent) {
-        context = parent.getContext();
-        Itemlist itemlist = items.get(idx);
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        String TAG = MainActivity.class.getSimpleName();
+        final Context context = viewGroup.getContext();
+        final Itemlist item = items.get(position);
 
-        if(view ==  null){
+        if(convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.cell,parent,false);
+            convertView = inflater.inflate(R.layout.gridcell, viewGroup, false);
+
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            TextView tradeNum = (TextView) convertView.findViewById(R.id.tradeNum);
+
+            tradeNum.setText(item.getTradeNum());
+            name.setText(item.getName());
+            Log.d(TAG, "getView() - [ "+position+" ] "+item.getName());
+
+        } else {
+            View view = new View(context);
+            view = (View) convertView;
         }
 
-        TextView nameText = view.findViewById(R.id.name);
-        TextView tradeNum = view.findViewById(R.id.tradeNum);
+        //각 아이템 선택 event
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, item.getTradeNum()+" 번 - "+item.getName()+" 입니당! ", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        nameText.setText(itemlist.getName());
-        tradeNum.setText(itemlist.getTradeNum());
-
-        return view;
+        return convertView;  //뷰 객체 반환
     }
 }
