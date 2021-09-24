22a12fdba69a8eb91bf95aed686991d6bc27da07
soleileun <raemong21@cau.ac.kr>
2021-09-24 15:39:34 +0900

    [Edit] edit Toolbar

--- app/src/main/java/com/example/my4weekschallenge/MainActivity.java
+++ app/src/main/java/com/example/my4weekschallenge/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.my4weekschallenge;
 
-import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,8 +18,7 @@ import android.widget.Toolbar;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.gridlayout.widget.GridLayout;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 /*
 *
@@ -53,9 +50,8 @@ public class MainActivity extends AppCompatActivity {
         baseLayout.setBackgroundColor(Color.rgb(255, 255, 255));
         setContentView(baseLayout, params);
 
-//        Toolbar toolbar = new Toolbar(getApplicationContext());
-//        Toolbar toolbar = new Toolbar(getBaseContext());
-//        Toolbar toolbar = new Toolbar(getApplication());
+
+
         // 툴바
         Toolbar toolbar = new Toolbar(this);
         toolbar.setTitle("보유종목(국내)");
@@ -69,20 +65,22 @@ public class MainActivity extends AppCompatActivity {
         // 툴바 좌측 아이콘 추가 (버튼으로 구현)
         // 다양한 시도 실패중... 하...
       //  toolbar.inflateMenu(R.menu.cstoolbar); // 서랍 아이콘으로 만들어버리기 ㅡㅡ
-        LinearLayout btnlayout = new LinearLayout(this);
-//        btnlayout.setLayoutParams(lp);
 
+        LinearLayout btnlayout = new LinearLayout(this);
+        btnlayout.setLayoutParams(new ActionBar.LayoutParams(
+                        ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, Gravity.RIGHT
+                ));
         Button btn1 = new Button(this);
         btn1.setText("검색");
-        btnlayout.setBackgroundColor(Color.rgb(121,222,10));
         btnlayout.addView(btn1);
-        //toolbar.addView(btn1);
         Button btn2 = new Button(this);
         btn2.setText("환경설정");
 //        btn2.setLayoutParams(lp);
-        btnlayout.addView(btn2);
         //toolbar.addView(btn2);
+        // btnlayout.setGravity(Gravity.END|Gravity.RIGHT);
+        btnlayout.addView(btn2);
         toolbar.addView(btnlayout);
+
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,36 +98,46 @@ public class MainActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT));
         scrollLayout.setOrientation(LinearLayout.HORIZONTAL);
         HorizontalScrollView horizontalScrollView = new HorizontalScrollView(this);
-        horizontalScrollView.setBackgroundColor(Color.rgb(1,23,45));
+        horizontalScrollView.setBackgroundColor(Color.LTGRAY);
 //        horizontalScrollView.setLayoutParams(new LinearLayout.LayoutParams(1000,160));
-        LinearLayout lin = new LinearLayout(this);
-        Button btn4 = new Button(this);
-        btn4.setText("bsbbasdfbadf");
-        Button btn5 = new Button(this);
-        btn5.setText("dmfaaaaaaaaaaaaaaafs");
-        Button btn3 = new Button(this);
-        btn3.setText("tlqkasdfasdfhdi");
-        lin.addView(btn4);
-        lin.addView(btn5);
-        lin.addView(btn3);
+        horizontalScrollView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
 
-        horizontalScrollView.addView(lin);
+        LinearLayout hscrollBtnLayout = new LinearLayout(this);
+        Button scrollBtn1 = new Button(this);
+        scrollBtn1.setText("최근조회종목");
+        Button scrollBtn2 = new Button(this);
+        scrollBtn2.setText("보유종목(국내)");
+        Button scrollBtn3 = new Button(this);
+        scrollBtn3.setText("보유종목(해외)");
+        Button scrollBtn4 = new Button(this);
+        scrollBtn4.setText("특징종목");
+        Button scrollBtn5 = new Button(this);
+        scrollBtn5.setText("기본그룹");
+
+        hscrollBtnLayout.addView(scrollBtn1);
+        hscrollBtnLayout.addView(scrollBtn2);
+        hscrollBtnLayout.addView(scrollBtn3);
+        hscrollBtnLayout.addView(scrollBtn4);
+        hscrollBtnLayout.addView(scrollBtn5);
+
+        horizontalScrollView.addView(hscrollBtnLayout);
         scrollLayout.addView(horizontalScrollView);
         //수평 스크롤 옆 X 버튼
         LinearLayout btnXLayout = new LinearLayout(this);
-
+        btnXLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
         Button btnX = new Button(this);
-        btnX.setText("X");
-        btnXLayout.addView(btnX);
         btnX.setHeight(50);
         btnX.setWidth(50);
+        btnX.setText("X");
+        btnXLayout.addView(btnX);
         btnXLayout.setGravity(Gravity.END);
 
         scrollLayout.addView(btnXLayout);
         baseLayout.addView(scrollLayout);
-
-
-
 
 //        View card = new View(this);
 //        card.setBackgroundColor(Color.CYAN);
@@ -167,17 +175,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView space = new TextView(this);
-        space.setTextSize(20);
-        baseLayout.addView(space);
-
         baseLayout.addView(new Button(this));
     // 셀을 만들어볼것인데 맘 처럼 잘 안됨 ㅠ
 
         LinearLayout bigLayout = new LinearLayout(this);
+        bigLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        LinearLayout modLayout = new LinearLayout(this);
+        LinearLayout smLayout = new LinearLayout(this);
+        LinearLayout smLayout2 = new LinearLayout(this);
+        LinearLayout firLayout = new LinearLayout(this);
+        Button stName = new Button(this);
+        stName.setBackgroundColor(Color.BLACK);
+        TextView stNum = new TextView(this);
+        stNum.setText("155,234");
+        stNum.setBackgroundColor(Color.YELLOW);
+        TextView stMemo = new TextView(this);
+        stMemo.setText("시세 ");
+        stMemo.setBackgroundColor(Color.GREEN);
+
+        Button stPrice = new Button(this);
+        stPrice.setBackgroundColor(Color.RED);
+        stPrice.setHeight(260);
+
+        Button stArrow = new Button(this);
+        stArrow.setBackgroundColor(Color.BLUE);
+        stArrow.setText("UP");
+        stArrow.setHeight(250);
 
 
 
+        Button stDiff = new Button(this);
+        stDiff.setText("stDiff");
+        Button stPer = new Button(this);
+        stPer.setText("%");
+        smLayout2.setOrientation(LinearLayout.VERTICAL);
+        smLayout2.addView(stDiff);
+        smLayout2.addView(stPer);
+
+        Button stBong = new Button(this);
+        stBong.setBackgroundColor(Color.GREEN);
+        stBong.setText("df");
+
+
+
+        smLayout.addView(stNum);
+        smLayout.addView(stMemo);
+        firLayout.setOrientation(LinearLayout.VERTICAL);
+        firLayout.addView(stName);
+        firLayout.addView(smLayout);
+
+        modLayout.addView(firLayout);
+        modLayout.addView(stPrice);
+        modLayout.addView(stArrow);
+        modLayout.addView(smLayout2);
+        modLayout.addView(stBong);
+        bigLayout.addView(modLayout);
+        baseLayout.addView(bigLayout);
 
 
     }
@@ -216,6 +271,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+
+
+    public float dpToPx(float dp){
+        float px =  TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp,
+                this.getResources().getDisplayMetrics());
+        return px;
+    }
+
+
 
 
 }
