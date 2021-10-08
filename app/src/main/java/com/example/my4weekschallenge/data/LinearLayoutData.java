a859468944394a745251aec698fa166e53bcb7a1
soleileun <raemong21@cau.ac.kr>
2021-10-08 17:22:50 +0900

    [todo] make object and use json parser

---
+++ app/src/main/java/com/example/my4weekschallenge/data/LinearLayoutData.java
@@ -0,0 +1,20 @@
+package com.example.my4weekschallenge.data;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class LinearLayoutData extends ViewData{
+    // 상속생각하면 protected
+    // 보안 api를 만들때필요없는 정보가 너무 많을 필요는 없다.
+    //json 하드코딩 ㄴㄴ 자ㅠㅇ롭게 변형 가능하게
+
+
+    String id;
+    String location;
+    ArrayList item;
+
+
+
+
+}
---
+++ app/src/main/java/com/example/my4weekschallenge/data/ViewData.java
@@ -0,0 +1,11 @@
+package com.example.my4weekschallenge.data;
+
+public class ViewData {
+
+    //ㅈㅏ료구조를 먼저 생각을 하면 바로 파싱이된다.
+    String id;
+    String location ;
+
+
+
+}
